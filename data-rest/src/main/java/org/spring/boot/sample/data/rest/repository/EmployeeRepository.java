6fad9abcb01efe58f5421886dd0339742f9a1d00
tutous <usluga@t-online.de>
2017-10-02 19:51:34 +0200

    paging and sorting in EmployeeRepository

--- data-rest/src/main/java/org/spring/boot/sample/data/rest/repository/EmployeeRepository.java
+++ data-rest/src/main/java/org/spring/boot/sample/data/rest/repository/EmployeeRepository.java
@@ -3,15 +3,23 @@ package org.spring.boot.sample.data.rest.repository;
 import java.util.UUID;
 
 import org.spring.boot.sample.data.rest.domain.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(excerptProjection = EmployeeResource.class)
-public interface EmployeeRepository extends CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends CrudRepository<Employee, Long>, PagingAndSortingRepository<Employee, Long> {
 
-	@Query(value = "select e from Employee e where e.uuid =:uuid")
-	public Employee findByUuid(@Param("uuid") UUID uuid);
+	@RestResource(path = "resource", rel = "resource")
+	@Query(value = "select e from Employee e where e.uuid =:resourceId")
+	public Employee findByResourceId(@Param("resourceId") UUID resourceId);
+
+	@RestResource(path = "lastNameStartsWith", rel = "lastNameStartsWith")
+	public Page<Employee> findByLastNameStartsWith(@Param("lastName") String name, Pageable p);
 
 }
